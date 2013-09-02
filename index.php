802920b7e1e04bbb4748cbdf1faaa22574e12852
lourdesg <lourdesg3005@hotmal.com>
2013-09-02 10:21:00 -0600

    validar el boton inicio
    ya se inicia sesion con el usuario y contraseña

---
+++ index.php
@@ -0,0 +1,175 @@
+<?php
+	include("conexion.php");
+	include("funciones.php");
+if ($_SERVER['REQUEST_METHOD']=='POST') { // ¿Nos mandan datos por el formulario?
+    include('php_lib/config.ini.php'); //incluimos configuración
+    include('php_lib/login.class.php'); //incluimos las funciones
+    $Login=new Login();
+    //si hace falta cambiamos las propiedades tabla, campo_usuario, campo_contraseña, metodo_encriptacion
+
+    //verificamos el usuario y contraseña mandados
+    if ($Login->login($_POST['nice'],$_POST['password'], $_POST['id_estado'], $_POST['id_cateuser'])) {
+
+       //acciones a realizar cuando un usuario se identifica
+       //EJ: almacenar en memoria sus datos completos, registrar un acceso en una tabla mysql
+       //Estas acciones se veran en los siguientes tutorianes en http://www.emiliort.com
+
+        //saltamos al inicio del área restringida
+        header('Location: pagina-acceso-restringido.php');
+        die();
+    } else {
+        //acciones a realizar en un intento fallido
+        //Ej: mostrar captcha para evitar ataques fuerza bruta, bloquear durante un rato esta ip, ....
+        //Estas acciones se veran en los siguientes tutorianes en http://www.emiliort.com
+
+        //preparamos un mensaje de error y continuamos para mostrar el formulario
+        $mensaje='Usuario o contraseña incorrecto.';
+    }
+} //fin if post
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+		<title>Base de Conocimiento Banadesa </title>
+		<script src="js/jquery.js"></script>
+		<script src="tiptip/jquery.tipTip.js"></script>
+		<script src="tiptip/jquery.tipTip.minified.js"></script>
+		<link href="tiptip/tipTip.css" rel="stylesheet">
+		<script src="usuarios/buscar.js"></script>
+		<script>
+			$(function(){	
+			$(".tiptip").tipTip();
+			});
+		</script>
+		
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="js/jquery.validate.js"></script>
+		<script>
+			$(function(){
+				$('#miForm1').validate();
+			}
+		</script>
+		<title>Base de Conocimiento</title>
+		<link href="Estilo/css/estilo.css" rel="stylesheet">
+		
+	<style type = "text/css">
+		a {
+			  color: blue;
+			  text-decoration: none;
+		}
+		a:hover {
+			   color: green;
+		 
+		}
+	</style>
+	<body>
+		<div class="container">
+				<div class="row">
+					<div id="banner">
+						<div class="span12">
+							<img class ="redondear" src="Imagenes/logoBanadesa.png" width="920px" height="150px"  border="0">
+							
+							
+						</div><!--cierra el div de la clase span12-->
+					</div><!--cierra el div del id banner-->
+				</div><!--cierra el div de la clase row-->
+				
+				<hr class= "linea" style=" margin-left:45px">
+		<table "width="920px"   cellpadding="0" >
+			<tr>
+				<td  width= "500px" height ="400px">
+					<img  class="redondear" src="Imagenes/Banadesa.png"width= "400px" height="400px">
+				</td>
+					<td  width= "600px" height="400px" valign="top" >
+						<h1 style="text-align: center;">
+							<font face="verdana,Comic Sans MS,arial" size=6>
+								<span style="color: black;">
+									Bienvenido a la Base de  
+								</span>
+								<br>
+								<br>
+								<span>Conocimiento de</span>
+								<br>
+								<br>
+								<span>Banadesa</span>
+							</font>	
+						</h1>
+						<form method="POST" action="">
+							<div class="formulario">
+									<table class="formulario"width= "340px" height="200px" valign="top">
+										<tr>
+										<font size =4>
+											<td style='font-size: 16px'>
+											<b>
+												Nombre de Usuario:
+											</b>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<input type="text" size="25" maxlength="25" valign="left"  name="nice" style='font-size: 16px'>
+											</td>
+										</font>
+										</tr>
+										
+										<tr>
+										<font size =4>
+											<td style='font-size: 16px'>
+											<b>
+												Contrase&ntilde;a:
+											</b>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<input type="password" size="25" maxlength="25" valign="left"   name="password" style='font-size: 16px'>
+											</td>
+										</font>
+										</tr>
+										
+										<tr>
+										<td class= "label" href="#" onclick=”#” style='color: blue; text-decitation: none; color: green' >
+											<a href="#"> <b> <u>&#191;Ha olvidado su contrase&ntildea?</u> </b></a>
+										</td>
+										</tr>
+									
+									</table>
+								</div>
+							
+								
+						<font size=6>
+						<input type="submit" name="iniciar" value="Iniciar Sesi&oacute;n" style='size=6;color:white;width:250px;font-size: 16px; height:50px; background-color: #669933' /> 
+						</font>
+			</form>	
+			</td>
+			</tr>
+			</table>
+			
+			<?php
+			if (isset($_POST['iniciar'])) {
+				require("login.php");
+			}
+			?>
+	</div><!--cierre de  container-->		
+			
+	
+		<br>
+			<center>
+			<table>
+				<tr>
+				<td align="center">
+						<small>
+						&copy; Banadesa <?php echo date("Y");?>
+						</small>
+				</td>
+				</tr>
+				</table>
+				<small>
+					Banco Nacional de Desarrollo Agricula
+				</small>
+			</center>
+	</body>
+</html>
